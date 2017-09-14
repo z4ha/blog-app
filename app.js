e93b909e54de78c5aaf71a0da3c3c16e1ab0d75c
z4ha <jakmusze@gmail.com>
2017-09-14 22:41:27 +0200

    Add express sanitizer

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var express     = require('express'),
-app             = express(),
-mongoose        = require('mongoose'),
-bodyParser      = require('body-parser'),
-methodOverride  = require('method-override');
+var express       = require('express'),
+app               = express(),
+mongoose          = require('mongoose'),
+bodyParser        = require('body-parser'),
+expressSanitizer  = require('express-sanitizer'),
+methodOverride    = require('method-override');
 
 // Database and app setup
 mongoose.Promise = global.Promise;
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost/blog_app', { useMongoClient: true});
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride('_method'));
 
 // Mongoose model config
@@ -45,6 +47,7 @@ app.get('/blogs/new', function(req, res){
 
 // Create route 
 app.post('/blogs', function(req, res){
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err, newBlog){
     if(err){
       res.render('new');
@@ -78,6 +81,7 @@ app.get('/blogs/:id/edit', function(req, res){
 
 // Update route
 app.put('/blogs/:id',function(req, res){
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
     if (err){
       res.redirect('/blogs');
