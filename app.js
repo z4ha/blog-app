9a962d1f0b903e89ba2406115c17c75950b02584
z4ha <jakmusze@gmail.com>
2017-09-14 21:04:25 +0200

    Add show route

--- app.js
+++ app.js
@@ -52,6 +52,17 @@ app.post('/blogs', function(req, res){
   })
 })
 
+// Show route
+app.get('/blogs/:id', function(req, res){
+  Blog.findById(req.params.id,function(err, foundBlog){
+    if (err){
+      res.redirect('/blogs')
+    } else{
+      res.render('show', {blog: foundBlog})
+    }
+  })
+})
+
 app.listen(3000, function(){
   console.log('Server is running on port 3000');
 })
